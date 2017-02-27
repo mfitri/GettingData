96c93529b8c70b7af03f069498e8e1deb76a38bf
mfitri <fitrimekan@gmail.com>
2017-02-27 12:34:32 +0800

    Update GettingData.R

--- GettingData.R
+++ GettingData.R
@@ -1,4 +1,4 @@
 library(readr)
-download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./labourforce.csv", "auto")
+download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./labourforce.csv")
 labour_force <- read_csv("./labourforce.csv")
 View(labour_force)
